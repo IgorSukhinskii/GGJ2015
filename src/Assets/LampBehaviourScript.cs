abde66cbe94943b7509d2972a5632a2252373af2
Игорь Сухинский <si1en7ium@gmail.com>
2015-01-24 13:17:00 +0500

    Можно приклеивать объекты к планетам

---
+++ src/Assets/LampBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LampBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var onPlanet = GetComponent<LamplighterBehaviourScript>();
+        var diff = onPlanet.planet.transform.rotation *
+            (new Vector3(0, 1, 0) * (onPlanet.planet.planetRadius + onPlanet.height));
+        var headPosition = onPlanet.planet.transform.position + diff;
+        // проверить, видно ли звезду (0, 0) из headPosition
+	}
+}
---
+++ src/Assets/LamplighterBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LamplighterBehaviourScript : MonoBehaviour {
+    public PlanetBehaviour planet;
+    public float height;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var diff = this.planet.transform.rotation *
+            (new Vector3(0, 1, 0) * (this.planet.planetRadius + height/2));
+        this.transform.position = this.planet.transform.position + diff;
+        this.transform.rotation = this.planet.transform.rotation;
+	}
+}
