afccc550073d897f18420b50f853b7e490c16ccd
Игорь Сухинский <si1en7ium@gmail.com>
2015-01-24 16:45:36 +0500

    В фонарщика добавлен угол.

--- src/Assets/LamplighterBehaviourScript.cs
+++ src/Assets/LamplighterBehaviourScript.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class LamplighterBehaviourScript : MonoBehaviour {
     public PlanetBehaviour planet;
     public float height;
+    public float angle;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        var diff = this.planet.transform.rotation *
+        var rotation = Quaternion.Euler(0, 0, this.planet.transform.rotation.eulerAngles.z + angle);
+        var diff = rotation *
             (new Vector3(0, 1, 0) * (this.planet.planetRadius + height/2));
         this.transform.position = this.planet.transform.position + diff;
-        this.transform.rotation = this.planet.transform.rotation;
+        this.transform.rotation = rotation;
 	}
 }
