b47160b2977db16effcac05a5e459def088643c3
Игорь Сухинский <si1en7ium@gmail.com>
2015-01-24 14:03:01 +0500

    Фонарь знает, светло ли

--- src/Assets/LampBehaviourScript.cs
+++ src/Assets/LampBehaviourScript.cs
@@ -13,7 +13,17 @@ public class LampBehaviourScript : MonoBehaviour {
         var onPlanet = GetComponent<LamplighterBehaviourScript>();
         var diff = onPlanet.planet.transform.rotation *
             (new Vector3(0, 1, 0) * (onPlanet.planet.planetRadius + onPlanet.height));
-        var headPosition = onPlanet.planet.transform.position + diff;
-        // проверить, видно ли звезду (0, 0) из headPosition
+        var headPosition3 = onPlanet.planet.transform.position + diff;
+        var headPosition = new Vector2(headPosition3.x, headPosition3.y);
+        var hit = Physics2D.Raycast(headPosition, Vector2.zero - headPosition,
+            Vector2.Distance(headPosition, Vector2.zero) + 0.01f);
+        if (hit.collider != null)
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
+        }
 	}
 }
