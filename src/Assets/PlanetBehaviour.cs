d107baaa331993de22d0112cef22d944ccf1031d
kazarey <kazarey@gmail.com>
2015-01-24 12:33:04 +0500

    Added planet orbital rotation

--- src/Assets/PlanetBehaviour.cs
+++ src/Assets/PlanetBehaviour.cs
@@ -7,18 +7,32 @@ public class PlanetBehaviour : MonoBehaviour {
     public float selfRotationSpeed;
     public float planetRadius;
     public float orbitalPosition;
-    public float selfRotationPosition;
+
 	// Use this for initialization
 	void Start () {
-        this.transform.Rotate(0, 0, selfRotationPosition);
-        this.transform.position = new Vector3(0, 0, 0);
+		this.transform.position = new Vector3(0, orbitRadius, 0);
         // посчитать x через радиус орбиты и текущий орбитальный угол, центр вращения -- 0;0
 
+		float rad = orbitalSpeed * Mathf.Deg2Rad;
+		Debug.Log(orbitalSpeed + "degrees are equal to " + rad + " radians.");
+
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
         float selfRotationCurrentSpeed = selfRotationSpeed * Time.deltaTime;
-        selfRotationPosition += selfRotationCurrentSpeed;
+		this.transform.Rotate(0, 0, selfRotationCurrentSpeed);
+
+		float orbitalCurrentSpeed = orbitalSpeed * Time.deltaTime;
+		float newX = Mathf.Cos (orbitalCurrentSpeed * Mathf.Deg2Rad)* this.transform.position.x
+					- Mathf.Sin (orbitalCurrentSpeed * Mathf.Deg2Rad)* this.transform.position.y;
+
+		float newY = Mathf.Sin (orbitalCurrentSpeed * Mathf.Deg2Rad)* this.transform.position.x
+					+ Mathf.Cos (orbitalCurrentSpeed * Mathf.Deg2Rad)* this.transform.position.y;
+		
+		this.transform.position = new Vector3(newX, newY, 0);
+
 	}
 }
