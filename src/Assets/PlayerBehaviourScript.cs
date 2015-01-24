e6534cf9ff54dfaf671ae0f9f5fe672f2b99c347
Игорь Сухинский <si1en7ium@gmail.com>
2015-01-24 17:37:42 +0500

    Player

---
+++ src/Assets/PlayerBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // move out
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //move in
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<LamplighterBehaviourScript>().planet.lamp.Switch();
+        }
+	}
+}
